ae51ed329c2bd84d21180821fc367e83d7d54c97
lznr <166831725+lznr@users.noreply.github.com>
2024-05-23 17:56:49 +0200

    Update Brett.php

--- Brett.php
+++ Brett.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <!-------------------->  
-  <title>Chess Game Board</title>
+  <title>Schachbrett</title>
   <style>
     /* Stil für das Schachbrett */
     .container {
